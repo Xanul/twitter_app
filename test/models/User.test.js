f8f7764f17cb833b4c1e07e4ebbd07040d85e917
Xanul <rivas.060@gmail.com>
2022-04-19 11:37:29 -0500

    Added req 3. testing getters

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -14,3 +14,14 @@ describe('Unit testing for User class', () => {
     
     })
 })
+
+describe('Unit testing for User class', () => {
+    test('Add getters', () => {
+        const user = new User(1, "carlogilmar", "Carlo", "Bio")
+        expect(user.getUsername).toBe("carlogilmar")
+        expect(user.getBio).toBe("Bio")
+        expect(user.getDateCreated).not.toBeUndefined()
+        expect(user.getLastUpdated).not.toBeUndefined()
+    
+    })
+})
