fb0532be78ebb9ad517bb0f95b7d1e36f185dadc
Xanul <rivas.060@gmail.com>
2022-04-19 13:03:30 -0500

    Added Sprint 2 req 2. testing getInfo static method

--- app/services/UserService.js
+++ app/services/UserService.js
@@ -3,9 +3,17 @@ const User = require("../models/User")
 class UserService {
 
     static create(id, username, name) {
-        return new User(id, username, name, "Sin bio")
+        return new User(id, username, name, "Sin Bio")
+    }
+
+    static getInfo(obj){
+        return Object.values(obj) //Este metodo devuelve un arreglo con los valores correspondites del objeto
     }
 }
+
+const user = UserService.create(2, "Xanul", "Rodrigo")
+const userInfoList = UserService.getInfo(user)
+// console.log(userInfoList)
 
 module.exports = UserService
 
--- test/services/UserService.test.js
+++ test/services/UserService.test.js
@@ -8,3 +8,14 @@ describe("Test for UserService", () => {
         expect(user.bio).not.toBeUndefined()
     })
 })
+
+describe("Test for UserService", () =>{
+    test("2. Test UserService static method", () => {
+        const user = UserService.create(2, "Xanul", "Rodrigo")
+        const userInfoList = UserService.getInfo(user)
+        expect(userInfoList[0]).toBe(2)
+        expect(userInfoList[1]).toBe("Xanul")
+        expect(userInfoList[2]).toBe("Rodrigo")
+        expect(userInfoList[3]).toBe("Sin Bio")
+    })
+})
