ac5094308e5fa2d1b176df2dd4d6bbc08e2bedec
Xanul <rivas.060@gmail.com>
2022-04-21 14:29:26 -0500

    Added Sprint 3 req 2. Testing null values inside payload object

--- app/views/UserView.js
+++ app/views/UserView.js
@@ -3,12 +3,25 @@ const UserService = require('./../services/UserService')
 class UserView {
     
     static createUser(payload){
-        if(payload === null) // Si el payload esta vacio entonces retorna un objeto con el error
-        return {error: "El payload no existe o no contiene informacion"}
+        if(payload === null || payload === undefined) {
+            return {error: "El payload no existe o no contiene informacion"}
+        } // Si el payload esta vacio entonces retorna un objeto con el error
+        else if (payload.username === null || payload.name === null || payload.id === null) {
+            return {error: "Se nececita un valor valido"}
+        }
+        else {
+            return payload
+        }
+        
     }
 
 }
 
-
+const testPayloadObject = {
+    username: "Xanul",
+    name: null,
+    id: "22"
+}
+console.log(UserView.createUser(testPayloadObject))
 
 module.exports = UserView
